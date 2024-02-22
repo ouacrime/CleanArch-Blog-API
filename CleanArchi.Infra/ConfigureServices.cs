88e0e0d2251a24e91ae9b7ccfe0fc9f93b62542b
othmane <othmaneouacrim@gmail.com>
2024-02-22 08:57:42 +0100

    Initial commit: Add CleanArch.API project files

---
+++ CleanArchi.Infra/ConfigureServices.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchi.Infra
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration) 
+        {
+            return services;
+        
+        }
+
+
+
+    }
+}
